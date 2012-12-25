5367db04a179f0b76f749613dc12d8f0816f355b
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-12-25 20:22:02 +0530

    Got command working without bash

--- main.go
+++ main.go
@@ -4,21 +4,22 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"strings"
 )
 
-func run(arg string) {
-	cmd := exec.Command("bash", "-c", arg)
+func run(arg ...string) {
+	//cmd := exec.Command("bash", "-c", arg)
+	log.Printf("running *%v*\n", arg)
+	cmd := exec.Command(arg[0], arg[1:]...)
 	out, err := cmd.Output()
 	if err != nil {
-	  log.Fatal("ERR", err)
+		log.Fatal("ERR", err)
 	}
-	log.Println(">"+ arg)
 	log.Println(string(out))
 }
 
 func main() {
 	log.Println("Started go cmdrunner")
-	run(strings.Join(os.Args[1:], " "))
+	log.Println(os.Args)
+	run(os.Args[1:]...)
 	log.Println("Finished go cmdrunner")
 }

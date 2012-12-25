7e7ae39aeea6b7facfb2433a8d982b9581531d24
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-12-25 19:49:47 +0530

    working version

--- main.go
+++ main.go
@@ -8,13 +8,17 @@ import (
 )
 
 func run(arg string) {
-	cmd := exec.Command(arg)
-	cmd.Start()
-	cmd.Wait()
+	cmd := exec.Command("bash", "-c", arg)
+	out, err := cmd.Output()
+	if err != nil {
+	  log.Fatal("ERR", err)
+	}
+	log.Println(">"+ arg)
+	log.Println(string(out))
 }
 
 func main() {
 	log.Println("Started go cmdrunner")
-	run(strings.Join(os.Args, " "))
+	run(strings.Join(os.Args[1:], " "))
 	log.Println("Finished go cmdrunner")
 }
